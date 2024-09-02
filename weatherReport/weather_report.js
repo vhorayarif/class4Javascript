5d96972aba3ec13d046f2b4e6f47258c3c95809f
vhorayarif <vhorayarif@gmail.com>
2024-09-01 20:27:02 -0400

    api key removed

--- weatherReport/weather_report.js
+++ weatherReport/weather_report.js
@@ -3,7 +3,7 @@ function showweatherDetails(event){
     event.preventDefault();
 
     const city = document.getElementById('city').value;
-    const apiKey = 'f51d30d214aa31a6f00042484aa703ee';
+    const apiKey = 'xxxxxxx';
     const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
 
     fetch(apiUrl)
